b9b51c9b941e41ac75fb6609e8c32558e5543b5b
vsamiamv <63832288+vsamiamv@users.noreply.github.com>
2025-06-26 12:54:26 -0400

    Update deploy_agw.ps1

--- deploy_agw.ps1
+++ deploy_agw.ps1
@@ -1,14 +1,14 @@
 $resourceGroup = "dvwa-rg"
 $agwPublicIP = "dvwa-agw-pip"
 $agwsku = "WAF_v2"
-$applicationGatewayName = "dvwa-agw"
+$applicationGatewayName = "dvwa-agw_v2"
 $location = "eastus"
 $vnet = "dvwa-vnet"
 $subnetAGW = "subnet-agw"
 # GET VM PRIVATE IP
 $vmPrivateIP = az vm list-ip-addresses --resource-group $resourceGroup --name $vmName --query "[0].virtualMachine.network.privateIpAddresses[0]" -o tsv
 $wafPolicy = "dvwa-waf-policy"
-$workspace = "waf-logs-workspace"
+$workspace = "waf-logs-v4"
 $diagnostic_logs = "diagnostic_logs"
 
 az login
@@ -30,17 +30,21 @@ az network application-gateway create `
 	--servers $vmPrivateIP `
 	--waf-policy $wafPolicy
 
-# create a log analytics workspace and link to the diagnostic in the application gateway
-az monitor log-analytics workspace create `
-    --resource-group $resourceGroup `
-    --workspace-name $workspace `
-    --location $location
+# link to the diagnostic setting in the application gateway
+
 az monitor diagnostic-settings create `
-    --name $diagnostic_logs `
-    --resource $applicationGatewayName `
-    --workspace $workspace `
-    --logs '[{"category": "ApplicationGatewayAccessLog", "enabled": true}, {"category": "ApplicationGatewayPerformanceLog", "enabled": true}, {"category": "ApplicationGatewayFirewallLog", "enabled": true}]' `
-    --metrics '[{"category": "AllMetrics", "enabled": true}]'
+  --name $diagnostic_logs `
+  --resource $(az network application-gateway show `
+                --name $applicationGatewayName `
+                --resource-group $resourceGroup `
+                --query id -o tsv) `
+  --workspace $workspace `
+  --logs '[{"category":"ApplicationGatewayAccessLog","enabled":true},{"category":"ApplicationGatewayPerformanceLog","enabled":true},{"category":"ApplicationGatewayFirewallLog","enabled":true}]' `
+  --metrics '[{"category":"AllMetrics","enabled":true}]'
+
+
+$agwIP = az network public-ip show --resource-group $resourceGroup --name $agwPublicIP --query "ipAddress" -o tsv
+Write-Host "DVWA will be accessible at: http://$agwIP/dvwa/setup.php"
 
 
 
